17abc195974e26226b5aec6ab6a526e726fda47e
Conan Truong <truong.conan@gmail.com>
2018-10-20 14:13:56 -0700

    Camera script

---
+++ Spiral/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    [SerializeField] GameObject follow;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - follow.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = follow.transform.position + offset;
+    }
+}
