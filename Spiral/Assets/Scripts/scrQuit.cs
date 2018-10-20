682a688a05bf70ba35905f6e75cabdac10954a01
Jesse Allas <jesseallas@gmail.com>
2018-10-20 11:02:24 -0700

    MainMenu
    I made a main menu

---
+++ Spiral/Assets/Scripts/scrQuit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrQuit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
+public class ExitOnClick : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
